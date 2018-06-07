acda3965ce7714daadb2511b9d65ddaac204c97a
Nikhil UK <mr.nikhiluk@gmail.com>
2018-06-07 20:35:26 +0530

    Update MoveDatabase.ps1

--- MoveDatabase.ps1
+++ MoveDatabase.ps1
@@ -3,8 +3,9 @@ $ErrorActionPreference = "Stop"
 $dataTo="F:\SQLDB\data\"
 $logTo="F:\SQLDB\log\"
 $instance="user"
-$dblist=invoke-sqlcmd -server user -database master -query "select DB_NAME(database_id) as dbname,name,file_id,physical_name from sys.master_files where physical_name like 'C:%' and database_id>4"
+$dblist=invoke-sqlcmd -server user -database master -query "select DB_NAME(database_id) as dbname,name,file_id,physical_name from sys.master_files where physical_name like 'E:%' and database_id>4"
 $dbs= $dblist.dbname | get-unique
+try{
 foreach($db in $dbs){
 $offlinequery="alter database [$db] set offline"
 invoke-sqlcmd -server $instance -database master -query $offlinequery
@@ -17,10 +18,15 @@ if($filename.file_id -eq 1){
 $physicalnewname=$dataTo+$physicalfile
 }
 elseif($filename.file_id -eq 2){
-$physicalnewname="$logTo"+"$physicalfile"
+$physicalnewname=$logTo+$physicalfile
 }
-$alterquery="ALTER DATABASE $db MODIFY FILE ('NAME =$logicalname, FILENAME =$physicalnewname');" 
+$alterquery="ALTER DATABASE $db MODIFY FILE (NAME =$logicalname, FILENAME ='$physicalnewname');" 
 Move-item  -path $physicalname -destination $physicalnewname
 invoke-sqlcmd -server $instance -database master -query $alterquery
 }
+$bringonline="ALTER DATABASE $db SET ONLINE;"
+invoke-sqlcmd -server $instance -database master -query $bringonline
+}
+}
+catch{
 }
